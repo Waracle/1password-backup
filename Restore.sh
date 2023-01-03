41dbf57a114c022aaa1f4470d68492bb363facca
CharlieSun-Waracle <120096245+CharlieSun-Waracle@users.noreply.github.com>
2023-01-03 12:22:04 +0000

    Add files via upload

---
+++ Restore.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+print_usage()
+{
+    echo "1Password Cloud Restore"
+    echo "usage: $0 -f <input file>"
+    exit 0
+}
+
+# define variables
+tool_op="op"
+tool_jq="jq"
+
+# parse arguments
+while getopts "f:" option
+do
+    case "${option}" in
+        f) var_inputfile=${OPTARG};;
+        *) print_usage
+    esac
+done
+
+# check arguments
+if [ -z "${var_inputfile}" ]; then print_usage; fi
+
+# decrypt and print to STDOUT
+openssl enc -d -aes256 -in ${var_inputfile} | ${tool_jq}
